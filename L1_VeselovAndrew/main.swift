864309d856cb508760dbd60fd33e47664eb72ca0
andrew veselov <andrew.veselov@gmail.com>
2019-03-16 22:24:47 +0500

    Initial Commit

---
+++ L1_VeselovAndrew/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L1_VeselovAndrew
+//
+//  Created by Andrew Veselov on 16/03/2019.
+//  Copyright © 2019 Andrew Veselov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
